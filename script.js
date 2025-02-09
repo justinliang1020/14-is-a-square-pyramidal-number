eeb7ce70b7468fa08871d1642152c71e3b6d626a
Justin Liang <justinliang1020@gmail.com>
2025-02-09 13:25:43 -0500

    update script.js to what is in index.html

--- script.js
+++ script.js
@@ -1,50 +1,28 @@
+// paste this into the script tag of `template.html`
 let n = 3;
-
 function modifyLevel(increase) {
-  const pyramids = document.getElementById("pyramids");
+  const levels = document.getElementById("levels");
   const total = document.getElementById("total");
   const equation = document.getElementById("equation");
   if (increase) {
     n += 1;
-    pyramids.appendChild(generateLevelPre(n));
+    const el = document.createElement("pre");
+    el.textContent = Array(n)
+      .fill()
+      .map(() => "꩜".repeat(n))
+      .join("\n");
+    levels.appendChild(el);
   } else if (n > 1) {
     n -= 1;
-    pyramids.removeChild(pyramids.lastElementChild);
+    levels.removeChild(levels.lastElementChild);
   }
-  equation.textContent = generateEquation(n);
-  total.textContent = getP(n);
+  equation.textContent =
+    Array(n)
+      .fill()
+      .map((_, i) => (i + 1) ** 2)
+      .join(" + ") + ` = ${getPyr(n)}`;
+  total.textContent = getPyr(n);
 }
-
-function getP(n) {
-  let res = 0;
-  for (i = 1; i <= n; i++) {
-    res += i * i;
-  }
-  return res;
-}
-
-function generateLevelPre(n) {
-  let res = "";
-  for (i = 1; i <= n; i++) {
-    res += "꩜".repeat(n);
-    if (i !== n) {
-      res += "\n";
-    }
-  }
-  const node = document.createElement("pre");
-  node.textContent = res;
-  return node;
-}
-
-function generateEquation(n) {
-  let res = "";
-  for (i = 1; i <= n; i++) {
-    res += i * i;
-    if (i !== n) {
-      res += " + ";
-    }
-  }
-  res += ` = ${getP(n)}`;
-
-  return res;
+function getPyr(n) {
+  return (n * (n + 1) * (2 * n + 1)) / 6;
 }
